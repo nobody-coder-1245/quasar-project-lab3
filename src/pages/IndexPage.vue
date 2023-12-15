7a709278c8761c4a3feca5b1862b3e7d5b063fa1
Traiphakh stihhikaew <sirprak1245@gmail.com>
2023-12-15 11:26:16 +0700

    add: chinese-lang input context

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -16,11 +16,11 @@
         filled
         type="number"
         v-model="age"
-        label="Your age *"
+        label="年龄 *"
         lazy-rules
         :rules="[
-          (val) => (val !== null && val !== '') || 'Please type your age',
-          (val) => (val > 0 && val < 100) || 'Please type a real age',
+          (val) => (val !== null && val !== '') || '请输入年龄',
+          (val) => (val > 0 && val < 100) || '请输入您的真实年龄。',
         ]"
       />
       <q-toggle v-model="accept" label="I accept the license and terms" />
